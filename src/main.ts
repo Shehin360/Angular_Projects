28b669f65c98fc5c11b90e6d5bc65c715dff5e8a
Shehin T Shaji <shehin@Shehins-MacBook-Air.local>
2025-06-28 19:46:58 +0530

    Added some files

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { appConfig } from './app/app.config';
+// import { App } from './app/app';
+// import { provideRouter } from '@angular/router';  
+// import { routes } from './app/app.routes';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+// bootstrapApplication(App, {
+//   providers: [provideRouter(routes)]
+// }).catch((err) => console.error(err));
+import { bootstrapApplication } from '@angular/platform-browser';
+import { App } from './app/app'; // Assuming you've renamed AppComponent to App
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+
+bootstrapApplication(App, {
+  providers: [provideRouter(routes)]
+}).catch((err) => console.error(err));
